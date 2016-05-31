aca3149f19040bef7de29fa6e4a57c7e01346bc3
nekko1119 <taku50501119@gmail.com>
2016-06-01 04:24:25 +0900

    Read config file and create if it is not exists

---
+++ HostsEditor/HostsEditorForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HostsEditor
+{
+    public partial class HostsEditorForm : Form
+    {
+        private string ConfigFileName { get; } = "hostspath.txt";
+        private string DefaultHostsPath { get; } = @"C:\Windows\System32\drivers\etc\hosts";
+        private string HostsPath { get; set; }
+
+        public HostsEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HostsEditor_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Text = "HostsEditor";
+                this.CreateConfigFileIfNotExists();
+                this.ReadConfigFile();
+            }
+            catch
+            {
+                this.Close();
+            }
+        }
+
+        private void CreateConfigFileIfNotExists()
+        {
+            if (!File.Exists(this.ConfigFileName))
+            {
+                using (var stream = File.Create(this.ConfigFileName))
+                {
+                    var writer = new StreamWriter(stream);
+                    writer.WriteLine();
+                }
+            }
+        }
+
+        private void ReadConfigFile()
+        {
+            var config = Enumerable.Empty<string>();
+            try
+            {
+                config = File.ReadLines(this.ConfigFileName);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "エラー", MessageBoxButtons.OK);
+                throw;
+            }
+            this.HostsPath = config.First();
+        }
+    }
+}
